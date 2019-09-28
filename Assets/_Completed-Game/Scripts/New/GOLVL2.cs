0c74e490f88d6f467970a5ede00dd1a47ac30adf
ItsukaKitty <kittyitsuka@gmail.com>
2019-09-28 15:34:30 +0630

    RollABall

---
+++ Assets/_Completed-Game/Scripts/New/GOLVL2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GOLVL2 : MonoBehaviour
+{
+    public void golvl2()
+    {
+        SceneManager.LoadScene("LVL 2");
+    }
+}
